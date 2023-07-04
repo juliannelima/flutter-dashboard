18efbf2589e1c865c2c061d8707b48ba4f101586
Julianne <juliannelicon@mpto.mp.br>
2023-07-04 16:16:47 -0300

    Cria widget drawer, main, Web e Mobile

--- lib/main.dart
+++ lib/main.dart
@@ -23,38 +23,92 @@ class MyApp extends StatelessWidget {
           ),
           title: const Text('Dashboard'),
         ),
-        body: Row(
-          children: [
-            Drawer(
-              child: ListView(
-                children: const [
-                  ListTile(
-                    leading: Icon(
-                      Icons.arrow_right_outlined,
-                      size: 32,
-                    ),
-                    title: Text('Menu'),
-                  ),
-                  ListTile(
-                    leading: Icon(
-                      Icons.arrow_right_outlined,
-                      size: 32,
-                    ),
-                    title: Text('Menu'),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                height: double.infinity,
-                color: Colors.blueGrey,
-                child: const Text('main'),
-              ),
-            ),
-          ],
-        ),
+        body: const Home(),
       ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    final media = MediaQuery.of(context).size.width >= 450;
+
+    return media ? const WebWidget() : const MobileWidget();
+  }
+}
+
+class WebWidget extends StatelessWidget {
+  const WebWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        DrawerWidget(),
+        Expanded(
+          child: MainWidget(),
+        ),
+      ],
+    );
+  }
+}
+
+class MobileWidget extends StatelessWidget {
+  const MobileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: double.infinity,
+      child: MainWidget(),
+    );
+  }
+}
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(
+              Icons.arrow_right_outlined,
+              size: 32,
+            ),
+            title: Text('Menu'),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.arrow_right_outlined,
+              size: 32,
+            ),
+            title: Text('Menu'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MainWidget extends StatelessWidget {
+  const MainWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      color: Colors.blueGrey,
     );
   }
 }
